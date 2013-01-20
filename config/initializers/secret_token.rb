e7a29c783a9d3d6ca1c407bbc40b08a746e0ab02
yokohama <yokohama@raw-hide.co.jp>
2013-01-20 21:53:21 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloword::Application.config.secret_token = 'c1bdbcca9db36e8da3683125f98543cbec8684142617d8e97725f0bc42be9d1fdee6455ceedefb3ae5077b61dbe46cbf39349a9ba662219c6dffd3209e0fb39a'
