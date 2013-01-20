630e8b3ba160cdf4d256ac0fa03375ec60f9ae76
yokohama <yokohama@raw-hide.co.jp>
2013-01-21 03:18:47 +0900

    swipeデザイン

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,6 @@
+#coding: utf-8
 class Word < ActiveRecord::Base
  attr_accessible :keyword, :discription
- validates_presence_of :keyword, :discription
+ validates_length_of :keyword, :within => 1..40, :message => "は1〜40文字以内で入力して下さい"
+ validates_length_of :discription, :within => 1..255, :message => "は1〜255文字以内で入力して下さい"
 end
