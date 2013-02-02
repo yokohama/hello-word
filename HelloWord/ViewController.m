edd9dd776eaadc241fe4ef1099a43f7c439b7168
yokohama <yokohama@raw-hide.co.jp>
2013-02-02 23:10:25 +0900

    Initial Commit

---
+++ HelloWord/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloWord
+//
+//  Created by 横濱 悠平 on 13/02/02.
+//  Copyright (c) 2013年 横濱 悠平. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
