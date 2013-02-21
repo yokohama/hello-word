b39225566972a2104ed1d674a16f6d26a7795a43
yokohama <yokohama@raw-hide.co.jp>
2013-02-21 15:27:53 +0900

    api実装

---
+++ app/controllers/api/users/sessions_controller.rb
@@ -0,0 +1,17 @@
+class Api::Users::SessionsController < Devise::SessionsController
+
+  # POST /resource/sign_in for iPhone & Android login
+  def create_by_mobile
+    user = params[:user]
+    @user = User.find_by_email user[:email]
+    if @user
+      if @user.valid_password? user[:password]
+        render :status=>200, :json=>{:message=>"Success"}
+      else
+        render :status=>400, :json=>{:message=>"Error :password"}
+      end
+    else
+      render :status=>400, :json=>{:message=>"Error :email"}
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Helloword::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {
+    sessions:'users/sessions'
+  }
 
-  #root :to => 'words#index'
-  root :to => 'dashboard#index'
+  #as :user do
+  #  post 'sign_in_by_mobile_player' => "users/sessions#create_by_mobile_player", as:'user_session_by_mobile_player'
+  #end
+
+  root to:'dashboard#index'
 
   match '/dashboard/:book_id' => 'dashboard#index'
 
@@ -13,6 +18,14 @@ Helloword::Application.routes.draw do
         get :swipe
         delete :destroy_all
         match '/iframe' => 'words#iframe'
+      end
+    end
+  end
+
+  as :user do
+    namespace :api do
+      namespace :users do
+        post 'sign_in_by_mobile' => "sessions#create_by_mobile"
       end
     end
   end
