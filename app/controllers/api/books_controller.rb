d495b9072144c7d94ab734f5e08c13c484b6fcd1
yokohama <yokohama@raw-hide.co.jp>
2013-02-25 16:45:36 +0900

    API側でログ出力実装

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -9,14 +9,17 @@ class Api::BooksController < ApplicationController
       if @user.valid_password? user[:password]
         books = [Book.library_new(@user)]
         words = []
+        logger.info("@user=#{@user.inspect}")
+        logger.info("books=#{@user.books.inspect}")
         @user.books.each do |b|
           books << b
         end
         json_books = []
         books.each do |b|
+          logger.info("words=#{b.as_json(:include => :words)}")
           json_books << b.as_json(:include => :words)
         end
-        #render status:200, :json=>{books:books}
+        logger.info("json_books=#{json_books.inspect}")
         render status:200, :json=>{books:json_books}
       else
         render status:400, :json=>{:message=>"Error :password"}
