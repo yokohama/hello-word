e7efa6d8a66889da1650f0834273391968029748
yokohama <yokohama@raw-hide.co.jp>
2013-03-01 19:22:44 +0900

    sample/fileにファイルアップロードのサンプル実装

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,4 +1,11 @@
 class SamplesController < ApplicationController
   def update
+    files = [];
+    f = params[:file]
+    #f.original_filename  # => ファイル名
+    #f.content_type       # => Content-Type
+    p ">>>>>> #{f.size}"
+    p Base64.decode64(f)
+    #p csv
   end
 end
