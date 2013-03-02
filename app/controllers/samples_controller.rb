98e33c8164e7050cce093222832f661a32b037c0
yokohama <yokohama@raw-hide.co.jp>
2013-03-02 23:41:49 +0900

    ファイルアップロードとりあえず動く段階。

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -2,24 +2,27 @@
 require "csv"
 
 class SamplesController < ApplicationController
+=begin
   def update
     a = params[:files].split(",")
     p 'hogehogehogoehgoehgoe'
     p a.last.encoding
     p Base64.decode64(a.last)
   end
+=end
 
   def update
-    files = [];
     f = params[:file]
-    g = Base64.decode64(f)
-    p g
-    p g.encoding
-=begin
-    csv = CSV.parse_line(g)
-    csv.each do |c|
-      p c
+    book = current_user.books.find params[:book_id]
+    csv = CSV.new(f)
+    book.words.each do |w|
+      #BUG:yokohama 関連も一緒に消す
+      w.destroy
     end
-=end
+    csv.each do |c|
+      word = Word.new :word => c[0], :answer => c[1]
+      word.user_id = current_user.id
+      book.words << word
+    end
   end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -6,7 +6,7 @@ class Word < ActiveRecord::Base
   has_many :book_words
   has_many :books, :through => :book_words
 
-  validates_length_of :word, :within => 1..40
-  validates_length_of :answer, :within => 1..255
+  #validates_length_of :word, :within => 1..40
+  #validates_length_of :answer, :within => 1..255
 
 end
