fa4f79fa3d4e2294ac4028a46cc1fc32570998a6
yokohama <yokohama@raw-hide.co.jp>
2013-03-05 18:26:46 +0900

    csvヴァリデーションのバグで詰まっている。なぜか、eachの中にはいってこない

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -99,4 +99,19 @@ class WordsController < ApplicationController
     @file = params[:file]
   end
 
+  def update
+    f = params[:file]
+    book = current_user.books.find params[:book_id]
+    csv = CSV.new(f)
+    book.words.each do |w|
+      #BUG:yokohama 関連も一緒に消す
+      w.destroy
+    end
+    csv.each do |c|
+      word = Word.new :word => c[0], :answer => c[1]
+      word.user_id = current_user.id
+      book.words << word
+    end
+  end
 end
+
